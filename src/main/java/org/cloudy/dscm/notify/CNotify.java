5f428a1a3730314e9d004606727ba9ccfa6d5d64
dragonmail2001 <dragonmail2001@163.com>
2016-06-09 16:03:20 +0800

    zjl

--- src/main/java/org/cloudy/dscm/notify/CNotify.java
+++
@@ -1,5 +0,0 @@
-package org.cloudy.dscm.notify;
-
-public interface CNotify<T> {
-	public boolean delivery(T message) throws Exception;
-}
--- src/main/java/org/cloudy/dscm/store/Test.java
+++
@@ -1,5 +0,0 @@
-package org.cloudy.dscm.store;
-
-public class Test {
-
-}
